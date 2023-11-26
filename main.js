2f55cc91def17f1ef396304f0600c83c7673f8d2
WroblewskiW <149715773+WroblewskiW@users.noreply.github.com>
2023-11-26 18:45:02 +0100

    createNewFile.js

---
+++ main.js
@@ -0,0 +1,11 @@
+alert(`Siema`);
+alert(`Jestem zajebisty`);
+
+const firstName =`Wojciech`;
+const age =32;
+
+console.log(`Nazywam się ${firstName} i mam ${age} lata.`);
+const wafeName =`Alina`;
+const liwingPlace =`Białystok`;
+const wafeAge = `30`;
+console.log(`Moja żona ma na imię ${wafeName} i mieszka razem ze mną w mieście o nazwie ${liwingPlace}. Moja żona ma ${wafeAge} lat.`);
